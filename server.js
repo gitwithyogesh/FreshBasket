55889fb02e4067be2bfbde2ae4b50011b46e525f
Yogesh Anup Shirke <yshirke005@gmail.com>
2026-03-03 21:30:29 +0530

    ,

--- server.js
+++ server.js
@@ -164,7 +164,7 @@ app.get('/api/orders/:email', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
