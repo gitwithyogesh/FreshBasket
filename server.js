b850c2ae178266f3a62a7a20743c30e9db289384
Yogesh Anup Shirke <yshirke005@gmail.com>
2026-03-03 18:40:07 +0530

    .

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
